bc438e22a3d55ba57017cdcef93180e0887c9b4c
Nikola Lukic <nlukic97@gmail.com>
2021-10-04 18:13:46 +0200

    api works, a found user data is replaced

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,53 @@
 async function queryUser(username){
     
     let url = 'https://api.github.com/users/' + username
-    let data = await fetch(url).then(response => response.json()).then(res=> {
+
+    // let data = 
+    let {
+            avatar_url:avatar, name, login, 
+            created_at, bio, public_repos: repos, 
+            followers, following, twitter, 
+            blog, location, company
+        } =
+    await fetch(url).then(response => response.json()).then(res=> {
         return res
-    })
+    });
 
-    console.log(data.name);
-    console.log(Date.parse(data.created_at));
-    console.log(data.login);
-    console.log(data.bio);
+    // console.log(created_at);
+    var fullDate = new Date(created_at)
+    let stringDate = `${fullDate.getDate()} ${fullDate.toLocaleDateString('default',{month:'short'})} ${fullDate.getFullYear()}`;
+    updateElement('#join-date',(stringDate)? stringDate : 'Unknown');
+    
 
-    console.log(data.repos);
-    console.log(data.followers);
-    console.log(data.following);
+    document.querySelector('#avatar').src = (avatar)? avatar : '';
+    
+    updateElement('#name',(name)? name : 'Unknown');
+    updateElement('#join-date',(created_at)? created_at : 'N/A');
+    updateElement('#login',(name)? '@'+login : 'Unknown');
+    updateElement('#bio',(bio)? bio : 'This profile has no bio');
 
-    console.log(data.twitter);
-    console.log(data.blog);
-    console.log(data.location);
-    console.log(data.company)
+    updateElement('#repos-number',(repos)? repos : '');
+    updateElement('#followers-number',(followers)? followers : '');
+    updateElement('#following-number',(following)? following : '');
+    
+    updateElement('#location',(location)? location : 'Not available');
+    updateElement('#twitter',(twitter)? twitter : 'Not Available');
+    updateElement('#blog',(blog)? blog : 'Not Available');
+    updateElement('#company',(company)? company : 'Not Available');
+
+
+
+
+
 }
 
 document.getElementById('search-btn').addEventListener('click',function(){
     let username = document.getElementById('username').value
     if(username != '' && username != null) queryUser(username)
 })
+
+function updateElement(querySelector, value){
+    document.querySelector(querySelector).innerText = value
+}
 
 queryUser('octocat')
