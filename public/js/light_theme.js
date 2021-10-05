d564e081bd02b386d71c33e69b21a43ca8eeeee5
Nikola Lukic <nlukic97@gmail.com>
2021-10-05 16:25:10 +0200

    method to change color scheme added. When body has class of 'dark' it will be in dark mode. Otherwise, it will be light

--- public/js/light_theme.js
+++ public/js/light_theme.js
@@ -2,3 +2,9 @@
 
 let mode = localStorage.getItem('dark-theme')
 console.log(mode);
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log('dark mode');
+} else {
+    console.log('light mode');
+}
